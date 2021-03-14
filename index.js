6a252334f198d260743bb2ff5103dcda2d51fcdd
t-panda <darkblue7x@gmail.com>
2021-03-14 22:44:08 +0300

    fixed navbar is added

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const sb = document.querySelector(".sidebar")
 const sb_close_btn = document.querySelector(".sidebar i")
 const sb_lst_btn = document.querySelectorAll(".sidebar__nav a")
 
+window.onscroll = function() {myFunction()};
+
+var navbar = document.querySelector(".navigation");
+var navbar_ofstY = navbar.offsetTop;
+
+function myFunction() {
+	if (window.pageYOffset >= navbar_ofstY) {
+		navbar.classList.add("navigation--fixed");
+	}
+	else {
+		navbar.classList.remove("navigation--fixed");
+	}
+}
+
 for (var i = 0; i < sb_lst_btn.length; i++) {
 	sb_lst_btn[i].addEventListener("click", (e) => {
 		var curr = document.querySelector(".sidebar__nav a.active")
